8f8d62244184d117288517dbe309736ac945559d
Chih-Yung Liang <stu89424@gmail.com>
2014-11-23 18:53:14 +0800

    Make finish a global.
    Because all of this program share only one finish semaphore.

--- main.c
+++ main.c
@@ -16,11 +16,9 @@ struct sort_range {
 };
 struct partition_arg {
 	int height;
-	sem_t *finish;
 	struct sort_range range;
 };
 struct sort_arg {
-	sem_t *finish;
 	struct sort_range range;
 };
 
@@ -28,6 +26,7 @@ void *partition(void *args);
 void *sort(void *args);
 
 int ary[1000000];
+sem_t finish;
 
 int main(int argc, char *argv[]) {
 	if(argc != 3) {
@@ -46,12 +45,10 @@ int main(int argc, char *argv[]) {
 	int count;
 	count = read_file(argv[1], ary);
 
-	sem_t finish;
 	sem_init(&finish, 0, 0);
 
 	struct partition_arg *init_arg = (struct partition_arg *) malloc(sizeof(struct partition_arg));
 	init_arg->height = 3;
-	init_arg->finish = &finish;
 	init_arg->range.head = 0, init_arg->range.tail = count - 1;
 
 	push_job(partition, init_arg);
@@ -96,7 +93,6 @@ void *partition(void *a) {
 			(struct partition_arg *) malloc(sizeof(struct partition_arg))
 		};
 
-		pass_arg[0]->finish = pass_arg[1]->finish = args->finish;
 		pass_arg[0]->height = pass_arg[1]->height = args->height;
 		pass_arg[0]->range = range[0];
 		pass_arg[1]->range = range[1];
@@ -109,7 +105,6 @@ void *partition(void *a) {
 			(struct sort_arg *) malloc(sizeof(struct sort_arg))
 		};
 
-		pass_arg[0]->finish = pass_arg[1]->finish = args->finish;
 		pass_arg[0]->range = range[0];
 		pass_arg[1]->range = range[1];
 
@@ -123,6 +118,6 @@ void *partition(void *a) {
 void *sort(void *a) {
 	struct sort_arg *args = (struct sort_arg *) a;
 	bsort(ary + args->range.head, args->range.tail - args->range.head + 1);
-	sem_post(args->finish);
+	sem_post(&finish);
 	free(args);
 }
